d0e37456c526c9f89dd7996c853ae08852e6be5f
AhmedTitef <57777850+AhmedTitef@users.noreply.github.com>
2020-12-08 21:54:03 -0800

    Initial commit

---
+++ src/EmailApp.java
@@ -0,0 +1,9 @@
+public class EmailApp {
+
+    public static void main(String[] args) {
+        Email email1 = new Email( "Ahmed" , "Elsayed" );
+        System.out.println(email1.showInfo());
+//        email1.setAlternateEmail( "js@gmail.com" );
+//        System.out.println(email1.getAlternateEmail());
+    }
+}
